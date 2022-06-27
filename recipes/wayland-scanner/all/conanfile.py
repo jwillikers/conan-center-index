9ec26f1b7492e39737531b89fae726a1c7e702b0
Jordan Williams <jordan@jwillikers.com>
2022-06-27 09:11:08 -0500

    wayland: Switch to MesonToolchain generator to allow cross-compiling

--- recipes/wayland-scanner/all/conanfile.py
+++ recipes/wayland-scanner/all/conanfile.py
@@ -9,10 +9,10 @@ from conans.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=1.43.0"
 
-class WaylandConan(ConanFile):
-    name = "wayland"
+class WaylandScannerConan(ConanFile):
+    name = "wayland-scanner"
     description = "Wayland is a project to define a protocol for a compositor to talk to its clients as well as a library implementation of the protocol"
-    topics = ("protocol", "compositor", "display")
+    topics = ("protocol", "compositor", "display", "wayland")
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://wayland.freedesktop.org"
     license = "MIT"
@@ -42,10 +42,7 @@ class WaylandConan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        if self.options.enable_dtd_validation:
-            self.requires("libxml2/2.9.14")
         self.requires("expat/2.4.8")
-        self.requires("libffi/3.4.2")
 
     def build_requirements(self):
         self.tool_requires("meson/0.62.2")
@@ -68,11 +65,11 @@ class WaylandConan(ConanFile):
         tc = MesonToolchain(self)
         tc.project_options["libdir"] = "lib"
         tc.project_options["datadir"] = "res"
-        tc.project_options["libraries"] = True
+        tc.project_options["libraries"] = False
         tc.project_options["dtd_validation"] = self.options.enable_dtd_validation
         tc.project_options["documentation"] = False
         if tools.Version(self.version) >= "1.18.91":
-            tc.project_options["scanner"] = False
+            tc.project_options["scanner"] = True
         tc.generate()
 
     def build(self):
@@ -89,44 +86,22 @@ class WaylandConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
-        self.cpp_info.components["wayland-server"].libs = ["wayland-server"]
-        self.cpp_info.components["wayland-server"].set_property("pkg_config_name", "wayland-server")
-        self.cpp_info.components["wayland-server"].names["pkg_config"] = "wayland-server"
-        self.cpp_info.components["wayland-server"].requires = ["libffi::libffi"]
-        self.cpp_info.components["wayland-server"].system_libs = ["pthread", "m"]
-        self.cpp_info.components["wayland-server"].resdirs = ["res"]
+        self.cpp_info.components["wayland-scanner"].set_property("pkg_config_name", "wayland-scanner")
+        self.cpp_info.components["wayland-scanner"].names["pkg_config"] = "wayland-scanner"
+        self.cpp_info.components["wayland-scanner"].resdirs = ["res"]
+        self.cpp_info.components["wayland-scanner"].requires = ["expat::expat"]
+        if self.options.enable_dtd_validation:
+            self.cpp_info.components["wayland-scanner"].requires.append("libxml2::libxml2")
         pkgconfig_variables = {
             'datarootdir': '${prefix}/res',
             'pkgdatadir': '${datarootdir}/wayland',
+            'bindir': '${prefix}/bin',
+            'wayland_scanner': '${bindir}/wayland-scanner',
         }
-        self.cpp_info.components["wayland-server"].set_property(
+        self.cpp_info.components["wayland-scanner"].set_property(
             "pkg_config_custom_content",
             "\n".join("%s=%s" % (key, value) for key,value in pkgconfig_variables.items()))
 
-        self.cpp_info.components["wayland-client"].libs = ["wayland-client"]
-        self.cpp_info.components["wayland-client"].set_property("pkg_config_name", "wayland-client")
-        self.cpp_info.components["wayland-client"].names["pkg_config"] = "wayland-client"
-        self.cpp_info.components["wayland-client"].requires = ["libffi::libffi"]
-        self.cpp_info.components["wayland-client"].system_libs = ["pthread", "m"]
-        self.cpp_info.components["wayland-client"].resdirs = ["res"]
-        pkgconfig_variables = {
-            'datarootdir': '${prefix}/res',
-            'pkgdatadir': '${datarootdir}/wayland',
-        }
-        self.cpp_info.components["wayland-client"].set_property(
-            "pkg_config_custom_content",
-            "\n".join("%s=%s" % (key, value) for key,value in pkgconfig_variables.items()))
-
-        self.cpp_info.components["wayland-cursor"].libs = ["wayland-cursor"]
-        self.cpp_info.components["wayland-cursor"].set_property("pkg_config_name", "wayland-cursor")
-        self.cpp_info.components["wayland-cursor"].names["pkg_config"] = "wayland-cursor"
-        self.cpp_info.components["wayland-cursor"].requires = ["wayland-client"]
-
-        self.cpp_info.components["wayland-egl"].libs = ["wayland-egl"]
-        self.cpp_info.components["wayland-egl"].set_property("pkg_config_name", "wayland-egl")
-        self.cpp_info.components["wayland-egl"].names["pkg_config"] = "wayland-egl"
-        self.cpp_info.components["wayland-egl"].requires = ["wayland-client"]
-
-        self.cpp_info.components["wayland-egl-backend"].names["pkg_config"] = "wayland-egl-backend"
-        self.cpp_info.components["wayland-egl-backend"].set_property("pkg_config_name", "wayland-egl-backend")
-        self.cpp_info.components["wayland-egl-backend"].version = "3"
+        bindir = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH environment variable: {}".format(bindir))
+        self.env_info.PATH.append(bindir)
--- recipes/wayland-scanner/all/test_package/conanfile.py
+++ recipes/wayland-scanner/all/test_package/conanfile.py
@@ -1,24 +1,20 @@
-import os
-
 from conan import ConanFile
 from conan.tools.build.cross_building import cross_building
-from conan.tools.cmake import CMake
+from conan.tools.gnu.pkgconfig import PkgConfig
 from conan.tools.layout import basic_layout
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+    generators = "PkgConfigDeps", "VirtualBuildEnv", "VirtualRunEnv"
 
     def layout(self):
         basic_layout(self)
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        pass
 
     def test(self):
         if not cross_building(self):
-            cmd = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(cmd, env="conanrun")
+            pkg_config = PkgConfig(self, "wayland-scanner", self.generators_folder)
+            self.run('%s --version' % pkg_config.variables["wayland_scanner"], env="conanrun")
