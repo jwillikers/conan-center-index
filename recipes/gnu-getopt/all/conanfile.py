c69d35784e11bbd9aba0b05fa150056b2d8b2073
Jordan Williams <jordan@jwillikers.com>
2024-04-16 09:44:52 -0500

    Don't use msys

--- recipes/gnu-getopt/all/conanfile.py
+++ recipes/gnu-getopt/all/conanfile.py
@@ -31,10 +31,6 @@ class GnuGetoptConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("meson/1.4.0")
-        if self._settings_build.os == "Windows":
-            self.win_bash = True
-            if not self.conf.get("tools.microsoft.bash:path", check_type=str):
-                self.tool_requires("msys2/cci.latest")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
